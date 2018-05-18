309d699ff626a08783c38af19f4d3f9e7e38e181
restuu <restu_u@outlook.com>
2018-05-18 20:56:16 +0700

    initial commit

---
+++ src/components/WorkList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="list mt-4 text-center">
+    <b-card :title="title">
+      <b-list-group v-for="item in items" :key="item">
+        <b-list-item>{{ item }}</b-list-item>
+      </b-list-group>
+    </b-card>
+  </div> 
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    items: Array
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="home" class="row">
+    <WorkList class="col-sm-4" title="ToDo"/>
+    <work-list class="col-sm-4" title="Work In Progress"/>
+    <work-list class="col-sm-4" title="Done"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import WorkList from '@/components/WorkList.vue'
+
+export default {
+  name: 'home',
+  components: {
+    WorkList
+  },
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
