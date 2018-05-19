caebbac19fce020f4fb2c526c2e659b8396d5662
restuu <restu_u@outlook.com>
2018-05-20 01:19:52 +0700

    real time database with firebase, todo, wip, done, -less delete

--- src/components/WorkList.vue
+++ src/components/WorkList.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="list mt-4 text-center">
-    <b-card :title="title">
-      <b-list-group v-for="item in items" :key="item">
-        <b-list-item>{{ item }}</b-list-item>
+    <b-card :title="title" :id="outline">
+
+      <b-list-group v-for="(value, key) in items" :key="key" class="row">
+        <b-list-group-item class="text-left p-1">
+          {{ value.name }} 
+          <b-btn class="p-1 btn" @click="$emit('trigger', key)" v-if="button">{{ button }}</b-btn>
+        </b-list-group-item>
       </b-list-group>
+
     </b-card>
   </div> 
 </template>
 
 <script>
+import { todo, wip, done } from '@/firebase.js'
+
 export default {
   props: {
     title: String,
-    items: Array
+    items: Object,
+    button: String
+  },
+
+  data () {
+    return {
+      outline: ''
+    }
+  },
+
+  mounted () {
+    if (this.title === 'ToDo') {
+      this.outline = 'red'
+    } else if (this.title === 'Work In Progress') {
+      this.outline = 'yellow'
+    } else {
+      this.outline = 'green'
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  #red {
+    border: 1px solid red 
+  }
+
+  #yellow {
+    border: 1px solid yellow
+  }
+
+  #green {
+    border: 1px solid green
+  }
+
+  .btn {
+    float: right;
+  }
+
+  .vertical {
+    vertical-align: middle //doesn't work
+  }
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,44 @@
 <template>
-  <div id="home" class="row">
-    <WorkList class="col-sm-4" title="ToDo"/>
-    <work-list class="col-sm-4" title="Work In Progress"/>
-    <work-list class="col-sm-4" title="Done"/>
+  <div id="home">
+    
+    <b-input-group class="col-sm-4 mx-auto mt-4">
+      <b-form-input placeholder="enter your task here" v-model="newTask"/>
+      <b-input-group-append>
+        <b-btn variant="info" @click="addTask">Add</b-btn>
+      </b-input-group-append>
+    </b-input-group>
+
+    <div id="worklist" class="row">
+      <WorkList 
+        class="col-sm-4" 
+        title="ToDo" 
+        :items="todos" 
+        button="Begin"
+        @trigger="startTodo"
+        />
+      <WorkList 
+        class="col-sm-4" 
+        title="Work In Progress" 
+        :items="wip"
+        button="Complete"
+        @trigger="completeTodo"
+        />
+      <WorkList 
+        class="col-sm-4" 
+        title="Done" 
+        :items="done"
+        />
+    </div>
+
+    <!-- {{ todos }} -->
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import WorkList from '@/components/WorkList.vue'
+import { todo, wip, done } from '@/firebase.js'
 
 export default {
   name: 'home',
@@ -18,8 +48,77 @@ export default {
 
   data () {
     return {
-
+      newTask: '',
+      todos: {},
+      wip: {},
+      done: {}
     }
+  },
+
+  methods: {
+    addTask () {
+      let todoObj = {
+        name: this.newTask,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        startedAt: null,
+        isCompleted: false
+      }
+      todo
+        .push(todoObj)
+        .then(snapshot => {
+          console.log('todo added to database')
+        })
+    },
+
+    startTodo (key) {
+      let self = this
+      todo.child(key).once('value')
+        .then(snapshot => {
+          let toBeStarted = snapshot.val()
+          toBeStarted.startedAt = Date.now()
+          console.log('---see toBeStarted', toBeStarted)
+          todo.child(key).remove()
+          wip.child(key).set(toBeStarted)
+            .then(err => {
+              if (err) {
+                console.log('error', err)
+              }
+              console.log('data moved to wip')
+            })
+        })
+    },
+
+    completeTodo (key) {
+      let self = this
+      wip.child(key).once('value')
+        .then(snapshot => {
+          let toBeCompleted = snapshot.val()
+          toBeCompleted.completedAt = Date.now()
+          console.log('---see toBeCompleted', toBeCompleted)
+          wip.child(key).remove()
+          done.child(key).set(toBeCompleted)
+            .then(err => {
+              if (err) {
+                console.log('error', err)
+              }
+              console.log('task completed')
+            })
+        })
+    }
+  },
+
+  created () {
+    let self = this
+    todo.on('value', function (snapshot) {
+      self.todos = snapshot.val()
+    })
+    wip.on('value', function (snapshot) {
+      self.wip = snapshot.val()
+    })
+    done.on('value', function (snapshot) {
+      self.done = snapshot.val()
+    })
   }
 }
 </script>
